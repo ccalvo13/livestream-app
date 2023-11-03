d735eb61525f55056f39898b8f6097998f504211
Chris David Calvo <ccalvo@startechup.com>
2023-11-03 12:34:21 +0800

    add commonjs

--- vite.config.js
+++ vite.config.js
@@ -78,6 +78,9 @@ export default defineConfig({
               // used during production bundling
               rollupNodePolyFill()
           ]
-      }
+      },
+      commonjsOptions: {
+        transformMixedEsModules: true,
+      },
   }
 })
