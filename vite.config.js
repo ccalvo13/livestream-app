8318113202c4068572174b9705eb1931287b2b2c
Chris David Calvo <ccalvo@startechup.com>
2023-11-03 12:00:32 +0800

    add commonjs

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,14 @@ import vue from '@vitejs/plugin-vue'
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
 import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
-
+import commonjs from '@rollup/plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    commonjs()
+  ],
   define: {
     'process.env': {},
     global: 'globalThis'
