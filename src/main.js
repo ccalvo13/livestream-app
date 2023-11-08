86032d652630d09ab648751d589e39a8e67d8b85
Chris David Calvo <ccalvo@startechup.com>
2023-11-08 10:16:18 +0800

    update vuetify

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 
 // Vuetify
 import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
+import { createVuetify, defaults as vuetifyDefaults } from 'vuetify'
 // import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
@@ -16,6 +16,7 @@ import * as directives from 'vuetify/directives'
 const vuetify = createVuetify({
     // components,
     directives,
+    defaults: vuetifyDefaults,
 })
 
 createApp(App).use(vuetify).mount('#app')
