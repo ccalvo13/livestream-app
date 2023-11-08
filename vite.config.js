698efe1fbbe3d34724acb26aef88b867b565d676
Chris David Calvo <ccalvo@startechup.com>
2023-11-08 10:27:04 +0800

    update vuetify

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
         template: {transformAssetUrls}
     }),
     viteCommonjs(),
+    vuetify(),
     federation({
       name: "livestream",
       filename: "livestreamApp.js",
