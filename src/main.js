7f426032f47485aa7093541338e52e1cff77c5c3
Chris David Calvo <ccalvo@startechup.com>
2023-11-08 10:10:49 +0800

    update vuetify and mdi

--- src/main.js
+++ src/main.js
@@ -8,18 +8,14 @@ import { createVuetify } from 'vuetify'
 // import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
-import 'vuetify/dist/vuetify.min.css'
+// import 'vuetify/dist/vuetify.min.css'
 // import '@mdi/font/css/materialdesignicons.css'
-import {mdi} from 'vuetify/lib/iconsets/mdi';
+// import {mdi} from 'vuetify/lib/iconsets/mdi';
 // import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 const vuetify = createVuetify({
     // components,
     directives,
-    icons: {
-        defaults: 'mdi',
-        sets: { mdi }
-    }
 })
 
 createApp(App).use(vuetify).mount('#app')
