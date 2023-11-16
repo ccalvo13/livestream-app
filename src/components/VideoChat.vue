a26671d7941a1639e90864ed75bfda4dc3f0a31e
Chris David Calvo <ccalvo@startechup.com>
2023-11-16 19:17:25 +0800

    update video chat css

--- src/components/VideoChat.vue
+++ src/components/VideoChat.vue
@@ -424,7 +424,7 @@
     height: 60px !important;
 }
 .video-webrtc-controls {
-    position: absolute;
+    position: relative;
     left: 0;
     right: 0;
     bottom: 60px;
@@ -435,10 +435,10 @@
 }
 
 .video-webrtc .video-item, .video-webrtc .video-item video {
-  width: 100vw;
+  width: 100%;
   height: 100vh;
   overflow: hidden;
-  background-size: cover;
+  /* background-size: cover; */
   object-fit: cover;
 }
 #call-canvas {
@@ -453,7 +453,7 @@
   order: 1;
   flex: 1;
   width: 100%;
-  position: absolute;
+  position: relative;
 }
 .video-item:not(:nth-child(1)) {
   width: 400px;
